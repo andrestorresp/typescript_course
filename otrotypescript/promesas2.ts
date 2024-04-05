fae8b9bd2fc768c9ccd9e792fe56c2acef993f5f
andrestorresp <dragotorres16@gmail.com>
2024-04-05 09:09:19 -0400

    actualizacion

---
+++ otrotypescript/promesas2.ts
@@ -0,0 +1,17 @@
+(()=>{
+    const retirarDinero = (montoRetirar: number) =>{
+       let dineroActual = 1000; 
+
+       return new Promise((resolve, reject) =>{
+        if (montoRetirar > dineroActual){
+            reject('No hay suficiente ahorros');
+        }else {
+            dineroActual -= montoRetirar;
+            resolve(dineroActual);
+        }
+       })
+    }
+    retirarDinero(1500)
+    .then(montoActual => console.log(`Me queda ${montoActual}`))
+    .catch(err=> console.warn(err))
+})();
